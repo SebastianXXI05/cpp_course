d24212dd1bfe8cc3fad7500c69896d93c43cdd35
Sebastian <Sebastian>
2023-03-31 10:02:46 -0500

    start c++ course

---
+++ syntax/hello.cpp
@@ -0,0 +1,23 @@
+#include <iostream> // Incluimos un header necesario para nuestro programa
+using namespace std; // Llama al compilador para que use el espacio de nombre "std"
+
+// la función min es donde el programa se ejecuta por primera vez
+int main() {
+  cout << "Hello World\n"; // imprime "Hello World" y un salto de linea con "\n"
+  return 0; // termina el programa y retorna 0
+}
+
+/*
+En c++ los identificadores son los nombres de las variables, los identificadores pueden iniciar
+con una letra minuscula, mayuscula o con un guión bajo y ir seguido de números, mas letras o
+guiones bajos, y no pueden tener nombres de palabras clave como "public". Ejemplo
+
+variable1, _hola, NumeRo.
+
+c++ es case-sensitive, es decir que diferencia entr minusculas y mayusculas como identificadores
+diferentes. Ejemplo
+
+Variable variable
+
+Son dos variables diferentes.
+*/

90dc1abc8ec0459c11511256273a370fcc42bddc
Sebastian <Sebastian>
2023-03-31 10:15:02 -0500

    comments added

---
+++ 01-comments/app.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+c++ soporta comentarios en linea y en bloque, estos comentarios sirven para dejar notas en nuestro
+codigo, estos comentarios van a ser ignorados por el compilador de c++.
+*/
+
+int main() {
+  // comentario en linea
+  /*
+    Comentario
+    en
+    bloque
+  */
+  cout <<"Hello World\n";
+  // el compilador ignora estas lineas
+  // cout <<"Hello user\n"; 
+  return 0;
+}
